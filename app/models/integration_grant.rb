828f8f48adc5df8be69b1978692042cfaf156839
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-28 14:58:46 +0100

    ✨ Add integration grant model

---
+++ app/models/integration_grant.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Records that a user has granted access to an integration
+class IntegrationGrant < ApplicationRecord
+  belongs_to :user
+
+  validates :provider, presence: true, inclusion: {
+    in: Rails.application.config.integrations.keys,
+  }
+  validates :domain, presence: true, inclusion: {
+    in: Rails.application.config.integrations.values.flat_map(&:keys),
+  }
+end
