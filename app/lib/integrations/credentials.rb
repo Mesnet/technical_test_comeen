37aa7274d6f622a5c999d90ab974fc5e4f1cba61
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-28 15:15:28 +0100

    ✨ Add credentials mapping class

---
+++ app/lib/integrations/credentials.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Integrations
+  class UnknownProviderError < StandardError; end
+
+  class Credentials
+    class << self
+      def resolve(provider, domain)
+        case provider.to_sym
+        when :google
+          Google::DelegatedTokenCredentials.new(domain)
+        else
+          raise UnknownProviderError, "Unknown provider: #{provider}"
+        end
+      end
+    end
+  end
+end
