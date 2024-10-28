5dcc924dc2190072172986962fabf6095634ae73
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-28 08:14:05 +0100

    🔧 Add config for google sheets integration

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module DeskBookingTest
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.google_sheets = config_for("external_services/google_sheets")
   end
 end
