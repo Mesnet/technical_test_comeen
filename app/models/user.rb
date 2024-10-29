07cd03a4c6cba8ba414a4c54c463192ce903a6e9
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 19:04:24 +0100

    ✨ Add access tokens associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable
 
+  # rubocop:disable Rails/InverseOf (the Doorkeeper's models do not have the corresponding associations)
+  has_many :access_grants,
+    class_name: "Doorkeeper::AccessGrant",
+    foreign_key: :resource_owner_id,
+    dependent: :delete_all # faster than :destroy since we're not running callbacks
+
+  has_many :access_tokens,
+    class_name: "Doorkeeper::AccessToken",
+    foreign_key: :resource_owner_id,
+    dependent: :delete_all # faster than :destroy since we're not running callbacks
+  # rubocop:enable Rails/InverseOf
+
   has_many :desk_bookings, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
