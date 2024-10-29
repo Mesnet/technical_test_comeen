c9663c83bb67872cc7f4186a1d29a175200fb4b7
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 16:51:52 +0100

    ✨ Add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include JSONAPI::Errors
+
   respond_to :json
+
+  def with_model_errors_handling
+    return unless block_given?
+
+    begin
+      yield
+    rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved, ActiveRecord::RecordNotDestroyed => e
+      render(jsonapi: e.record.errors, status: :unprocessable_entity)
+    end
+  end
+
+  def current_user
+    current_resource_owner
+  end
 end
