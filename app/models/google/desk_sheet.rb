f006627c24b88393a497530124aee1e87cff490c
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 09:47:41 +0100

    🐛 Fix dependency issue in dev

--- app/models/google/desk_sheet.rb
+++ app/models/google/desk_sheet.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_dependency "google"
+
 module Google
   class DeskSheet < ApplicationRecord
     validates :google_sheet_id, presence: true, uniqueness: true
