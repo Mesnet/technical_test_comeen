15d6fe012ae9795301d8575e58cfda121ed36b45
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 19:58:08 +0100

    🐛 Fix booking assistant not raising error properly

--- app/lib/desks/booking/assistant.rb
+++ app/lib/desks/booking/assistant.rb
@@ -2,8 +2,6 @@
 
 module Desks
   module Booking
-    class NoDeskAvailableError < StandardError; end
-
     class Assistant
       attr_reader :user, :starting, :ending
 
@@ -13,15 +11,31 @@ module Desks
         @ending = ending_datetime
       end
 
-      def book(desk: nil)
+      def book!(desk: nil)
         with_available_desk(desk: desk) do |desk|
-          DeskBooking.create(user:, desk: desk, start_datetime: starting, end_datetime: ending)
+          DeskBooking.create!(user:, desk: desk, start_datetime: starting, end_datetime: ending)
         end
+      rescue ActiveRecord::RecordInvalid => e
+        raise Desks::Booking::Errors::DeskBookingError.new(e.record), "Could not save booking due to validation errors"
+      end
+
+      def book(desk: nil)
+        book!(desk: desk)
+      rescue Desks::Booking::Errors::DeskBookingError => e
+        e.record
       end
 
       def with_available_desk(desk: nil)
+        passed_desk_occupied = desk && booked_desks.exists?(id: desk.id)
+
         desk ||= select_random_available_desk
-        raise "No available desks" unless desk
+        if passed_desk_occupied
+          raise Desks::Booking::Errors::NoDeskAvailableError.new(desk_booking_for_error(desk:)),
+            "The selected desk is already booked"
+        elsif desk.nil?
+          raise Desks::Booking::Errors::NoDeskAvailableError.new(desk_booking_for_error(desk:)),
+            "No desk available"
+        end
 
         yield desk
       end
@@ -36,6 +50,18 @@ module Desks
 
       def booked_desks
         Desk.joins(:desk_bookings).merge(DeskBooking.overlapping(starting, ending))
+      end
+
+      def desk_booking_for_error(desk:)
+        record = DeskBooking.new(user:, desk:, start_datetime: starting, end_datetime: ending)
+        record.valid?
+        if desk.nil?
+          record.errors.add(:desk, "no desk available")
+        else
+          record.errors.add(:desk, "is not available")
+        end
+
+        record
       end
     end
   end
