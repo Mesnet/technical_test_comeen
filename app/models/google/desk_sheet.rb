d0453be13047c8787488931f88aaf902ec1de702
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 21:23:29 +0100

    ✨ Add desks association

--- app/models/google/desk_sheet.rb
+++ app/models/google/desk_sheet.rb
@@ -5,5 +5,10 @@ require_dependency "google"
 module Google
   class DeskSheet < ApplicationRecord
     validates :google_sheet_id, presence: true, uniqueness: true
+
+    has_many :desks,
+      foreign_key: :google_desk_sheet_id,
+      dependent: :nullify,
+      inverse_of: :google_desk_sheet
   end
 end
