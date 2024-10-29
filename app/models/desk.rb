0cac391ae0a14d01aa49980937c921cf11197329
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 10:10:39 +0100

    🐛 Fix class name

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Desk < ApplicationRecord
-  belongs_to :google_desk_sheet, optional: true
+  belongs_to :google_desk_sheet, optional: true, class_name: "Google::DeskSheet"
   has_many :desk_bookings, dependent: :destroy
 
   validates :name, presence: true
