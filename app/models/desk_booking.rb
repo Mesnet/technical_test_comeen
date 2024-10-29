4fa6c28d427ee19d2d158e04f95d2e2cc01d8530
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 09:29:50 +0100

    ✨ Add overlapping desk bookings scope

--- app/models/desk_booking.rb
+++ app/models/desk_booking.rb
@@ -5,4 +5,14 @@ class DeskBooking < ApplicationRecord
   belongs_to :user
 
   validates :start_datetime, :end_datetime, presence: true
+
+  scope :overlapping, ->(start_datetime, end_datetime) do
+    where(
+      "`start_datetime` BETWEEN :starting AND :ending OR " \
+        "`end_datetime` BETWEEN :starting AND :ending OR " \
+        "(`start_datetime` < :starting AND `end_datetime` > :ending)",
+      starting: start_datetime,
+      ending: end_datetime,
+    )
+  end
 end
