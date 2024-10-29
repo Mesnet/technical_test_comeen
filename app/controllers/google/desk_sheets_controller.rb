f47372d199abde47260a85bc982243645fd3c8c4
Gaétan Basile <gaetan.basile@mink-agency.com>
2024-10-29 21:03:03 +0100

    ♻️ User synchroniser class to fetch & commit changes

--- app/controllers/google/desk_sheets_controller.rb
+++ app/controllers/google/desk_sheets_controller.rb
@@ -29,26 +29,31 @@ module Google
     end
 
     def list_sync_changes
-      @desks_from_sheet = @desk_sheet.fetch_sync_changes
+      @desks_from_sheet = Google::Sheets::Synchronizer.new(
+        Integrations.for(current_user, :google, :sheets).credentials,
+        @desk_sheet,
+      ).fetch_sync_changes
 
       render(jsonapi: @desks_from_sheet)
     end
 
     def commit_sync
-      @desks_from_sheet = @desk_sheet.fetch_sync_changes
-      with_model_errors_handling do
-        Desk.transaction do
-          @desks_from_sheet.each(&:save!)
-        end
+      @desks_from_sheet = Google::Sheets::Synchronizer.new(
+        Integrations.for(current_user, :google, :sheets).credentials,
+        @desk_sheet,
+      ).apply_sync_changes!
 
-        render(jsonapi: @desks_from_sheet)
-      end
+      render(jsonapi: @desks_from_sheet)
     end
 
     private
 
     def desk_sheet_params
       params.require(:desk_sheet).permit(:google_sheet_id) # replace with actual attributes
+    end
+
+    def load_desk_sheet
+      @desk_sheet = DeskSheet.find(params[:id])
     end
   end
 end
